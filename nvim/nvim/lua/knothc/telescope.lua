4c76776bd3ee545721b6db66c329d8959e1296dc
Efrain Mejia <knothc@macthing.lan>
2023-08-08 11:27:55 +0800

    Restored AWOL file

---
+++ nvim/nvim/lua/knothc/telescope.lua
@@ -0,0 +1,32 @@
+local status_ok, telescope = pcall(require, "telescope")
+if not status_ok then
+    return
+end
+
+local actions = require("telescope.actions")
+local action_layout = require("telescope.actions.layout")
+telescope.setup{
+  defaults = {
+    mappings = {
+      i = {
+        ["<esc>"] = actions.close,
+        ["<M-p>"] = action_layout.toggle_preview,
+      },
+      n = {
+        ["<M-p>"] = action_layout.toggle_preview,
+      },
+    },
+  },
+  pickers = {
+    find_files = {
+--      theme = "ivy",
+    },
+    buffers = {
+      theme = "dropdown",
+    },
+    treesitter = {
+      theme = "ivy",
+    },
+  },
+}
+
