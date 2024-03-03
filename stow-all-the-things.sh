eff424e141d83aa4e488f8055db8edbe58d76407
Efrain Mejia <knot.efrain@gmail.com>
2024-03-03 23:42:47 +0800

    Add stow-all-the-things script

---
+++ stow-all-the-things.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+stow -t ~ zsh -v
+stow -t ~/.config nvim -v
+stow -t ~/.config ranger -v
+stow -t ~/.config skhd -v
+stow -t ~/.config tmux -v
+stow -t ~/.config yabai -v
+stow -t ~/.config starship -v
